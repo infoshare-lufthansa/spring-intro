036abde9d534338bce072ed3f16da19e857348c3
maciej.koziara <maciej.koziara@airhelp.com>
2021-05-16 17:31:08 +0200

    Init commit

---
+++ src/main/java/pl/infoshare/springintro/SpringIntroExerciseApplication.java
@@ -0,0 +1,12 @@
+package pl.infoshare.springintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroExerciseApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/infoshare/springintro/SpringIntroExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.infoshare.springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
