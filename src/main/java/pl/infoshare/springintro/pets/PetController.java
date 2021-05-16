6ce68c248c78d73d7a97ba24a703c7d1717f521b
maciej.koziara <maciej.koziara@airhelp.com>
2021-05-16 17:37:16 +0200

    1st exercise solution

--- src/main/java/pl/infoshare/springintro/pets/PetController.java
+++ src/main/java/pl/infoshare/springintro/pets/PetController.java
@@ -5,6 +5,7 @@ import pl.infoshare.springintro.pets.model.Pet;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -12,13 +13,14 @@ import java.util.stream.Collectors;
 @RestController
 public class PetController {
 
+    private final AtomicInteger idGenerator = new AtomicInteger(1);
     private final Map<Integer, Pet> pets = new HashMap<>();
 
     @GetMapping("/api/pets")
     public Collection<Pet> getPets(@RequestParam(defaultValue = "") String q) {
         return pets.values()
                 .stream()
-                .filter(p -> p.getName().contains(q))
+                .filter(p -> p.getName().toUpperCase().contains(q.toUpperCase()))
                 .collect(Collectors.toList());
     }
 
